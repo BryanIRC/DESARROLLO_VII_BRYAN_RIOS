7eba38e8835c5d2b7eafcef9adb5db4b7ec40829
BryanIRC <brios.16@gmail.com>
2024-09-03 18:56:58 -0500

    Creación del directorio y archivos utilidades_texto.php y archivo analisis_texto.php

---
+++ PARCIALES/PARCIAL_1/analisis_texto.php
@@ -0,0 +1,2 @@
+<?php
+$frases = ["Created to create", "Si hay que arrojar la toalla que sea en la playa", "Draco dormiens nunquam titilandus"];
---
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -0,0 +1,16 @@
+<?php
+function contar_palabras($texto)
+{
+    $nPalabras = 0;
+    return $nPalabras;
+}
+function contar_vocales($texto)
+{
+    $nVocales = 0;
+    return $nVocales;
+}
+function invertir_palabras($texto)
+{
+    $nuevoTexto = "";
+    return $nuevoTexto;
+}
